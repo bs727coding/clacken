a9a41ed26e175cb437e6741dbce41e9eb15111ee
Carter Pollan <carter.pollan@gmail.com>
2025-01-24 19:14:37 -0700

    trying despratly to get zmk studio to work. It really might just implode and thats ok. I'm ok. Everything is ok.

--- build.bash
+++ build.bash
@@ -5,7 +5,7 @@ cp -r ~/Projects/keyboards/zmk_clacken/boards/shields/clacken ~/Projects/zmk/app
 #DIR=|pwd
 cd ~/Projects/zmk/app
 source ../.venv/bin/activate
-west build -p -d build/left -b seeeduino_xiao_ble -- -DSHIELD=clacken_left
+west build -p -d build/left -b seeeduino_xiao_ble -- -DSHIELD=clacken_left -S studio-rpc-usb-uart -DCONFIG_ZMK_STUDIO=y
 cp ./build/left/zephyr/zmk.uf2 ~/Projects/keyboards/zmk_clacken/build/left.uf2
-west build -p -d build/right -b seeeduino_xiao_ble -- -DSHIELD=clacken_right
-cp ./build/right/zephyr/zmk.uf2 ~/Projects/keyboards/zmk_clacken/build/right.uf2
+# west build -p -d build/right -b seeeduino_xiao_ble -- -DSHIELD=clacken_right
+#cp ./build/right/zephyr/zmk.uf2 ~/Projects/keyboards/zmk_clacken/build/right.uf2
